31937416dc14faa09884785e1744e46f7b1ce1b6
cernanb <cernanb@gmail.com>
2020-10-09 13:43:01 -0700

    Add ability to login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
         </Switch>
       </Router>
     </div>
--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -5,11 +5,32 @@ export const signup = (user, history) => {
       headers: {
         'Content-Type': 'application/json',
       },
+      credentials: 'include',
       body: JSON.stringify({ user: user }),
     })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        dispatch({
+          type: 'AUTH_SUCCESSFUL',
+          payload: { loggedIn: data.logged_in, currentUser: data.user },
+        });
+        history.push(`/dashboard`);
+      });
+  };
+};
+export const login = (user, history) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3001/sessions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ user: user }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
         dispatch({
           type: 'AUTH_SUCCESSFUL',
           payload: { loggedIn: data.logged_in, currentUser: data.user },
